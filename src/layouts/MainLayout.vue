90cabaacec5f04d95f84dea4500b86728adc61a9
andrey <A_polo91@mail.ru>
2020-05-08 21:42:56 +0300

    add localization

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,7 @@
     <Loader v-if="loading"/>
     <div class="app-main-layout" v-else>
       <Navbar v-on:click="isOpen = !isOpen"/>
-      <Sidebar v-model="isOpen"/>
+      <Sidebar v-model="isOpen" :key="locale"/>
       <main class="app-content" :class="{ full: !isOpen }">
         <div class="app-page">
           <router-view/>
@@ -37,9 +37,17 @@
     computed: {
       error() {
         return this.$store.getters.error;
+      },
+      locale(){
+        return this.$store.getters.info.locale
       }
     },
     watch: {
+      // locale(){
+      //
+      // },
+
+
       error(fbError) {
         this.$error(messages[fbError.code] || "Что-то пошло не так");
         console.log(fbError);
